c54a83231009f8194f9a24bb2d4d147496683d9c
Avinash Adluri <avinash@letsweel.com>
2025-04-01 18:43:38 +1100

    manifest fix

--- next.config.js
+++ next.config.js
@@ -1,12 +1,16 @@
-const withPWA = require('next-pwa')({
-  dest: 'public',
+const withPWA = require("next-pwa")({
+  dest: "public",
   register: true,
   skipWaiting: true,
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  // Remove or comment out the output: 'export' line if you want to use next start
+  // output: 'export',
+
+  // Or keep it and use a static file server instead
+
   eslint: {
     ignoreDuringBuilds: true,
   },
